847288b4c920d436b9e0c96ef2bd98721440e742
vero-1770 <verolm2006@gmail.com>
2026-06-18 19:45:04 -0300

    cors...

--- src/app.js
+++ src/app.js
@@ -18,7 +18,8 @@ app.use(cors({
     origin: [
       process.env.FRONTEND_URL, 
       process.env.VITE_API_URL, 
-      "http://localhost:5173"
+      "http://localhost:5173",
+      "http://localhost:3000"
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
