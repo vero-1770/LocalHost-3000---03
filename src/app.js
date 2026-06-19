eb65716331de2e8eeb885aeef89b71804ad3aa19
vero-1770 <verolm2006@gmail.com>
2026-06-19 15:18:02 -0300

    Fix error de tipeo en app.js

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ app.use(cors({
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
-}));det=
+}));
 
 app.use(express.json());
 app.use(cookieParser());
